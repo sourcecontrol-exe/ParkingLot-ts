684e5741a3b81f61ae9ca9169b13b3f9a9b80ccc
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-16 21:07:05 +0530

    update

--- app/index.ts
+++ app/index.ts
@@ -2,5 +2,6 @@ import * as http from 'http';
 const server = http.createServer(function(req,res){
 	res.write("hello world");
 	res.end()
+	console.log("hello this is server ruinning");
 })
 server.listen(8000);
