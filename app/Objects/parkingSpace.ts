944775d5ff349d2631dfab4e09a77643e0fa5c4f
Swetabh Subham <swetabh.subham@gmail.com>
2021-06-17 04:03:00 +0530

    update

--- app/Objects/parkingSpace.ts
+++ app/Objects/parkingSpace.ts
@@ -1,19 +1,61 @@
-import { parse } from "path/posix";
+class Car {
+	C_number: string;
+	C_color: string;
+	C_slot: number;
+	constructor(C_color: string, C_number: string, C_slot: number) {
+		this.C_color = C_color;
+		this.C_number = C_number;
+		this.C_slot = C_slot
+	}
+
+	isEqual(carA: Car, CarB: Car) {
+		return ((carA.C_number.toLowerCase() === CarB.C_number.toLowerCase()) &&
+			carA.C_color.toLowerCase() == CarB.C_color.toLowerCase())
+	}
+
+}
 
 class ParkingSpace {
 	MaxParkingSlots: number;
-	ParkingSlots;
+	ParkingSlots: any;
+	slotsOccupied: number
 	constructor() {
 		this.MaxParkingSlots = 0;
-		this.ParkingSlots = new Map()
+		this.slotsOccupied = 0;
+		this.ParkingSlots = new Set()
 	}
 
-	createpParkingLot(input: string){
-		this.MaxParkingSlots  = parseInt(input.split(" ")[1]);
-		if(this.MaxParkingSlots <= 0){
-			throw new Error("Minimum One slot is required");
+	createParkingLot(input: string) {
+		let space = parseInt(input.split(" ")[1]);
+		if (space > 0) this.MaxParkingSlots = space;
+		else throw new Error(" Please enter a valid Space for the parking Lot")
+	}
+
+	registerCarEntry(input: string) {
+		if (this.MaxParkingSlots <= 0) {
+			throw new Error("Please Create Space for Parking lot")
 		}
-		
+
+		let C_color = input.split(" ")[2];
+		let C_number = input.split(" ")[1];
+		let C_Slot = this.ParkingSlots.size()
+		if(this.slotsOccupied < this.MaxParkingSlots){
+			let car = new Car(C_color,C_number,C_slot);
+		}
+
+	}
+
+	leaveCarByNumber(input: string) {
+
+	}
+
+	getSlotDetails(input: string) {
+
+	}
+
+
+	findNearestSlot() {
+
 	}
 
 }
